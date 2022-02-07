057e5582c6d89f5cb0a66faf5ebb309303d788e7
Volodymyr Dvorskyi <feowurd@gmail.com>
2022-02-07 02:16:16 +0200

    complete filter panel

--- src/components/FilterPanel/FilterPanel.jsx
+++ src/components/FilterPanel/FilterPanel.jsx
@@ -3,25 +3,25 @@ import s from './FilterPanel.module.css';
 
 export default function FilterPanel() {
   return (
-    <Section>
-      <form action="">
-        <select>
-          <option value="value1">value 1</option>
-          <option value="value2">value 2</option>
-          <option value="value3">value 3</option>
-        </select>
-        <select>
-          <option value="value1">value 1</option>
-          <option value="value2">value 2</option>
-          <option value="value3">value 3</option>
-        </select>
-        <select>
-          <option value="value1">value 1</option>
-          <option value="value2">value 2</option>
-          <option value="value3">value 3</option>
-        </select>
-        <button type="submit"></button>
-      </form>
-    </Section>
+    <form className={s.filter__form} action="">
+      <select className={s.filter__select}>
+        <option value="value1">Filter 1</option>
+        <option value="value2">value 2</option>
+        <option value="value3">value 3</option>
+      </select>
+      <select className={s.filter__select}>
+        <option value="value1">Filter 2</option>
+        <option value="value2">value 2</option>
+        <option value="value3">value 3</option>
+      </select>
+      <select className={s.filter__select}>
+        <option value="value1">Filter 3</option>
+        <option value="value2">value 2</option>
+        <option value="value3">value 3</option>
+      </select>
+      <button className={s.filter__button} type="submit">
+        Apply
+      </button>
+    </form>
   );
 }
